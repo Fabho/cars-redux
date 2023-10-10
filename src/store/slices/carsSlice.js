c0e2fe7517dccdec199d5c853b5a3e6bbfcfafad
fabian.calsina@gmail.com <fabian.calsina@gmail.com>
2023-10-10 15:34:15 -0400

    Add redux logic for app

---
+++ src/store/slices/carsSlice.js
@@ -0,0 +1,33 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const carsSlice = createSlice({
+  name: "cars",
+  initialState: {
+    searchTerm: "",
+    data: [],
+  },
+  reducers: {
+    changeSearchTerm(state, action) {
+      state.searchTerm = action.payload;
+    },
+    addCar(state, action) {
+      // Assumption payload has the vals
+      state.data.push({
+        id: nanoid(),
+        name: action.payload.name,
+        cost: action.payload.cost,
+      });
+    },
+    removeCar(state, action) {
+      // Action apyload is id to remove
+      const updated = state.data.filter((car) => {
+        return car.id !== action.payload;
+      });
+
+      state.data = updated;
+    },
+  },
+});
+
+export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
+export const carsReducer = carsSlice.reducer;
